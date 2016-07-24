413b20a5a0e3229768420c150f66c132f416d07f
Timothée Floure <timothee.floure@fnux.ch>
2016-07-24 20:43:03 +0200

    Check power supply in power-check.rb

--- scripts/power-check.rb
+++ scripts/power-check.rb
@@ -1,5 +1,8 @@
 #!/usr/bin/env ruby
 
+# AC
+powerSupply = File.new("/sys/class/power_supply/BAT0/subsystem/AC/online", "r").gets.to_i
+
 # BAT0
 bat0Full = File.new("/sys/class/power_supply/BAT0/energy_full", "r").gets.to_i
 bat0Now = File.new("/sys/class/power_supply/BAT0/energy_now", "r").gets.to_i
@@ -10,21 +13,33 @@ bat1Full = File.new("/sys/class/power_supply/BAT1/energy_full", "r").gets.to_i
 bat1Now = File.new("/sys/class/power_supply/BAT1/energy_now", "r").gets.to_i
 bat1Inst = File.new("/sys/class/power_supply/BAT1/power_now", "r").gets.to_i
 
+# Computations
 powerFull = bat0Full + bat1Full
 powerNow = bat0Now + bat0Now
 percentage = (powerNow.to_f / powerFull.to_f) * 100
 
 instUsage = (bat0Inst + bat1Inst) / 1000.0**2
 
-if percentage < 20
+if powerSupply != 0
+  powerSupplyText = "online"
+else
+end
+
+if powerSupply != 0
+  powerSupplyText = "online"
+  level = "online"
+elsif percentage < 20
   level = "critical"
+  powerSupplyText = "offline"
 else
   level = "normal"
+  powerSupplyText = "offline"
 end
 
 system "
   notify-send -u #{level} '
   Actual power state : #{percentage.to_i}%
   Instantaneous : #{instUsage}W
+  Power Supply : #{powerSupplyText}
   '
 "
