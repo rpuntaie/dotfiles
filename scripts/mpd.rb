fc33430e1123ccfe312f36bbf11f369ea830c327
Timothée Floure <timothee.floure@fnux.ch>
2017-01-28 16:27:31 +0100

    Remove old useless scripts, clean some others

--- scripts/mpd.rb
+++ scripts/mpd.rb
@@ -7,7 +7,6 @@ mpd = MPD.new 'localhost', 6600
 mpd.connect
 
 song = mpd.current_song
-system "notify-send -u normal 'Current Song: #{song.artist} - #{song.title}'"
+system "notify-send -u normal 'Music Player Daemon - Playing' '#{song.artist} - #{song.title}'"
 
-# Disconnect
 mpd.disconnect
