4aef11fb1c68e4fc02a99dd1c5da2fd49d459011
Timothée Floure <timothee.floure@fnux.ch>
2017-01-28 16:20:13 +0100

    Rewrite backlight script : ruby -> bash

--- scripts/backlight.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env ruby
-
-maxBrightness = File.open("/sys/class/backlight/intel_backlight/max_brightness", "r").gets.to_i
-currentBrightness = File.open("/sys/class/backlight/intel_backlight/actual_brightness", "r").gets.to_i
-
-percentage = (currentBrightness.to_f / maxBrightness.to_f) * 100
-
-system "notify-send -u low 'Current Brightness : #{percentage.to_i}%'"
