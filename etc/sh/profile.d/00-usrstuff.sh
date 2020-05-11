3e280ff4e8bc9208dfa2c104bd765a1161a7f4b2
Roland Puntaier <roland.puntaier@gmail.com>
2020-05-12 00:12:08 +0200

    export defines

--- etc/sh/profile.d/00-usrstuff.sh
+++ etc/sh/profile.d/00-usrstuff.sh
@@ -1,12 +1,12 @@
 
 # user data separate directories
 # ~/.gnupg, ~/.password-store, ~/my and these ~/dotfiles
-MY="${HOME}/my"
-TASKDATA="${MY}/task"
-FINANCE="${MY}/finance"
+export MY="${HOME}/my"
+export TASKDATA="${MY}/task"
+export FINANCE="${MY}/finance"
 
 # environment variables usrstuff:
-NAME="Roland Puntaier"
-EMAIL="roland.puntaier@gmail.com"
-MYGPG="F85CB631327ED8DCD50C9FD3BC9A552643B86228"
+export NAME="Roland Puntaier"
+export EMAIL="roland.puntaier@gmail.com"
+export MYGPG="F85CB631327ED8DCD50C9FD3BC9A552643B86228"
 
--- etc/sh/profile.d/10-font.sh
+++ etc/sh/profile.d/10-font.sh
@@ -1,6 +1,6 @@
 #if not super user:
 #Couldn't get a file descriptor referring to the console
-FNTCONSOLE=/usr/share/kbd/consolefonts/ter-powerline-v14b.psf.gz
+export FNTCONSOLE=/usr/share/kbd/consolefonts/ter-powerline-v14b.psf.gz
 if [[ -f $FNTCONSOLE ]]; then
     setfont $FNTCONSOLE
 fi
