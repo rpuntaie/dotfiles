2b3e3ac4f62291c695497d62299e204aadf50a3d
Roland Puntaier <roland.puntaier@gmail.com>
2023-02-05 19:26:19 +0100

    recent changes

--- etc/sh/profile.d/00-usrstuff.sh
+++ etc/sh/profile.d/00-usrstuff.sh
@@ -14,3 +14,7 @@ export GPGKEY="F85CB631327ED8DCD50C9FD3BC9A552643B86228"
 export MWNOTMUCHNEW=0
 
 scp1(){scp $2 roland@192.168.1.$1:/home/roland/tmp/}
+
+export OPENAI_API_TOKEN="$(pass show platform.openai.com/apikey)"
+export INVOKEAI_ROOT="$HOME/invokeai"
+
