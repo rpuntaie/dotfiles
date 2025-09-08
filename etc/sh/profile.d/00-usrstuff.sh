71229e2246b2829d0a0fed9a1ba2bab4e13d11c5
Roland Puntaier <roland.puntaier@gmail.com>
2025-09-08 08:50:25 +0200

    misc

--- etc/sh/profile.d/00-usrstuff.sh
+++ etc/sh/profile.d/00-usrstuff.sh
@@ -6,6 +6,7 @@ export $(envsubst < $MY/.env)
 
 export OPENAI_API_TOKEN="$(pass show platform.openai.com/apikey)"
 export OPENAI_KEY="$(pass show platform.openai.com/apikey)"
+export GEMINI_API_KEY="$(pass show google/GEMINI_API_KEY)"
 export INVOKEAI_ROOT="$HOME/invokeai"
 export GH_TOKEN="$(pass show token_github.com/rpuntaie)"
 export Y2="$(date +%y)"
